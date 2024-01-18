cdfc22a629d5bc3082774d5c32ce027827cbb9ba
NgigiN <ngigi.nyongo@gmail.com>
2024-01-18 15:33:00 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -54,12 +54,12 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         sys.exit(1)
 
-    try:
-        N = int(sys.argv[1])
-        if N < 4:
-            raise ValueError("N must be at least 4")
-    except ValueError:
+    N = int(sys.argv[1])
+    if N < 4:
         print("N must be at least 4")
+        sys.exit(1)
+    if N.isdigit() is False:
+        print("N must be a number")
         sys.exit(1)
 
     solutions = solve_n_queens(N)
