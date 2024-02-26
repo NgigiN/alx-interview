44284eca4ed3fa73c3d2cb6c460af6841396e534
NgigiN <ngigi.nyongo@gmail.com>
2024-02-26 16:26:23 +0300

    Coin counter interview question

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+determines the number of coins needed to meet a given amount
+"""
+
+
+def makeChange(coins, total):
+    """
+    returns the fewest number of coins neede to meet a given amount
+
+    Args:
+            coins: list of the values of the coins in your possession
+            total: the amount to be met
+    """
+
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    coin_counter = 0
+
+    for coin in coins:
+        while total >= coin:
+            total = total - coin
+            coin_counter = coin_counter + 1
+    return coin_counter
